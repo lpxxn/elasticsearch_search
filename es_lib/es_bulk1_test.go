933efbfffa5631ca1447688035d5a43a528df052
lipeng <lipeng@meican.com>
2020-11-28 12:21:30 +0800

    feat: bulk test

--- es_lib/es_bulk1_test.go
+++ es_lib/es_bulk1_test.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"testing"
 )
+// curl -X DELETE "localhost:9200/bulk_index_1?pretty"
 
 func TestBulkIndex1(t *testing.T) {
 	indexName := "bulk_index_1"
@@ -24,19 +25,20 @@ func TestBulkIndex1(t *testing.T) {
 		Name: "li",
 		Age:  1,
 	}, &body{
-		ID:   1,
+		ID:   2,
 		Name: "zhang",
 		Age:  2,
 	}, &body{
 		ID:   3,
 		Name: "wang",
 		Age:  3,
+	}, &body{
+		ID:   4,
+		Name: "zhao",
+		Age:  4,
 	})
-	count := 1000
 	batch := 255
-	var buf bytes.Buffer
-	var numBatches int
-	var currBatch int
+	buf := new(bytes.Buffer)
 	for i, v := range bodyArray {
 		meta := []byte(fmt.Sprintf(`{ "index" : { "_id" : "%d" } }%s`, v.ID, "\n"))
 		data, err := json.Marshal(v)
@@ -48,25 +50,23 @@ func TestBulkIndex1(t *testing.T) {
 		buf.Write(meta)
 		buf.Write(data)
 		// When a threshold is reached, execute the Bulk() request with body from buffer
-		if i > 0 && i%batch == 0 || i == count-1 {
-			doBulk(currBatch, numBatches, buf, indexName, documentType)
+		if i > 0 && i%batch == 0 {
+			doBulk(buf, indexName, documentType)
 		}
 	}
 	if buf.Len() > 0 {
-		doBulk(currBatch, numBatches, buf, indexName, documentType)
+		doBulk(buf, indexName, documentType)
 	}
 	fmt.Println(buf.Len())
-	fmt.Print("\n")
+	fmt.Println()
 	log.Println(strings.Repeat("▔", 65))
-
 }
 
-func doBulk(currBatch int, numBatches int, buf bytes.Buffer, indexName string, documentType string) {
-	fmt.Printf("[%d/%d] ", currBatch, numBatches)
-
+func doBulk(buf *bytes.Buffer, indexName string, documentType string) {
+	fmt.Printf("bytes: %s \n", buf.String())
 	res, err := ES7ClientT.Bulk(bytes.NewReader(buf.Bytes()), ES7ClientT.Bulk.WithIndex(indexName), ES7ClientT.Bulk.WithDocumentType(documentType))
 	if err != nil {
-		log.Fatalf("Failure indexing batch %d: %s", currBatch, err)
+		log.Fatalf("Failure indexing batch  %#v", err)
 	}
 	if res.IsError() {
 		var raw map[string]interface{}
@@ -106,6 +106,7 @@ func doBulk(currBatch int, numBatches int, buf bytes.Buffer, indexName string, d
 	// Reset the buffer and items counter
 	buf.Reset()
 }
+
 
 type bulkResponse struct {
 	Errors bool `json:"errors"`
