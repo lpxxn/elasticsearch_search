f413e84815e6f9637456ba1ad08ae2005e16af4b
Aiden <mi_duo@live.com>
2021-12-21 16:06:32 +0800

    test

--- es_lib/goe_demo/restaurant_geo_shape_test.go
+++ es_lib/goe_demo/restaurant_geo_shape_test.go
@@ -19,8 +19,8 @@ func TestEnvelope1(t *testing.T) {
 					"geo_shape": map[string]interface{}{
 						"hotel.location": map[string]interface{}{
 							"shape": map[string]interface{}{
-								"type": "envelope",
-								"coordinates": [][]float64{[]float64{116.49036,39.97665}, {116.49211,39.97584}},
+								"type":        "envelope",
+								"coordinates": [][]float64{[]float64{116.49036, 39.97665}, {116.49211, 39.97584}},
 							},
 						},
 					},
@@ -42,7 +42,6 @@ func TestEnvelope1(t *testing.T) {
 	t.Log(string(b))
 }
 
-
 func TestPolygonEdit1(t *testing.T) {
 	queryBody := map[string]interface{}{
 		"query": map[string]interface{}{
@@ -57,11 +56,11 @@ func TestPolygonEdit1(t *testing.T) {
 							"shape": map[string]interface{}{
 								"type": "polygon",
 								"coordinates": [][][]float64{[][]float64{
-									[]float64{116.49114,39.97654},
-									[]float64{116.49118,39.97621},
-									[]float64{116.49118,39.97585},
-									[]float64{116.49208,39.97659},
-									[]float64{116.49114,39.97654},
+									[]float64{116.49114, 39.97654},
+									[]float64{116.49118, 39.97621},
+									[]float64{116.49118, 39.97585},
+									[]float64{116.49208, 39.97659},
+									[]float64{116.49114, 39.97654},
 								}},
 							},
 						},
@@ -98,8 +97,8 @@ func TestPolygonCircle1(t *testing.T) {
 					"geo_shape": map[string]interface{}{
 						"hotel.location": map[string]interface{}{
 							"shape": map[string]interface{}{
-								"type": "circle",
-								"radius": "500m",
+								"type":        "circle",
+								"radius":      "200m",
 								"coordinates": []float64{116.49158, 39.97626},
 							},
 						},
@@ -111,6 +110,73 @@ func TestPolygonCircle1(t *testing.T) {
 	// []float64{
 	//  北°, 东
 
+	resp, err := ES7ClientT.Search(ES7ClientT.Search.WithIndex("mytest_geo1"),
+		ES7ClientT.Search.WithContext(context.Background()),
+		ES7ClientT.Search.WithSize(200), ES7ClientT.Search.WithPretty(),
+		ES7ClientT.Search.WithBody(es7util.NewJSONReader(queryBody)),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp.String())
+	b, _ := json.Marshal(queryBody)
+	t.Log(string(b))
+}
+
+// 和上面是circle 等价
+func TestRestaurantDistance1(t *testing.T) {
+	queryBody := map[string]interface{}{
+		"query": map[string]interface{}{
+			"bool": map[string]interface{}{
+				"must": map[string]interface{}{
+					"match_all": struct{}{},
+				},
+				"filter": map[string]interface{}{
+					"geo_distance": map[string]interface{}{
+						"distance":       "200m",
+						"hotel.location": []float64{116.49158, 39.97626},
+					},
+				},
+			},
+		},
+	}
+	resp, err := ES7ClientT.Search(ES7ClientT.Search.WithIndex("mytest_geo1"),
+		ES7ClientT.Search.WithContext(context.Background()),
+		ES7ClientT.Search.WithSize(200), ES7ClientT.Search.WithPretty(),
+		ES7ClientT.Search.WithBody(es7util.NewJSONReader(queryBody)),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp.String())
+	b, _ := json.Marshal(queryBody)
+	t.Log(string(b))
+}
+
+// geo_distance 查询并排序，返回距离相隔多少米
+func TestRestaurantDistance2(t *testing.T) {
+	queryBody := map[string]interface{}{
+		"query": map[string]interface{}{
+			"bool": map[string]interface{}{
+				"must": map[string]interface{}{
+					"match_all": struct{}{},
+				},
+				"filter": map[string]interface{}{
+					"geo_distance": map[string]interface{}{
+						"distance":       "500m",
+						"hotel.location": []float64{116.49158, 39.97626},
+					},
+				},
+			},
+		},
+		"sort": map[string]interface{}{
+			"_geo_distance": map[string]interface{}{
+				"hotel.location": []float64{116.49158, 39.97626},
+				"order":          "asc",
+				"unit":           "m",
+			},
+		},
+	}
 	resp, err := ES7ClientT.Search(ES7ClientT.Search.WithIndex("mytest_geo1"),
 		ES7ClientT.Search.WithContext(context.Background()),
 		ES7ClientT.Search.WithSize(200), ES7ClientT.Search.WithPretty(),
