f07359283becc3b8dc9d82a6c8d9cb1c3572cc37
Aiden <mi_duo@live.com>
2021-12-21 15:47:07 +0800

    test

--- es_lib/goe_demo/restaurant_geo_shape_test.go
+++ es_lib/goe_demo/restaurant_geo_shape_test.go
@@ -43,7 +43,7 @@ func TestEnvelope1(t *testing.T) {
 }
 
 
-func TestPlygonedit1(t *testing.T) {
+func TestPolygonEdit1(t *testing.T) {
 	queryBody := map[string]interface{}{
 		"query": map[string]interface{}{
 			"bool": map[string]interface{}{
@@ -53,9 +53,9 @@ func TestPlygonedit1(t *testing.T) {
 				"filter": map[string]interface{}{
 					"geo_shape": map[string]interface{}{
 						"hotel.location": map[string]interface{}{
-							"relation": "within",
+							//"relation": "within",
 							"shape": map[string]interface{}{
-								// "type": "polygon",
+								"type": "polygon",
 								"coordinates": [][][]float64{[][]float64{
 									[]float64{116.49114,39.97654},
 									[]float64{116.49118,39.97621},
@@ -63,6 +63,44 @@ func TestPlygonedit1(t *testing.T) {
 									[]float64{116.49208,39.97659},
 									[]float64{116.49114,39.97654},
 								}},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	// []float64{
+	//  北°, 东
+
+	resp, err := ES7ClientT.Search(ES7ClientT.Search.WithIndex("mytest_geo1"),
+		ES7ClientT.Search.WithContext(context.Background()),
+		ES7ClientT.Search.WithSize(200), ES7ClientT.Search.WithPretty(),
+		ES7ClientT.Search.WithBody(es7util.NewJSONReader(queryBody)),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp.String())
+	b, _ := json.Marshal(queryBody)
+	t.Log(string(b))
+}
+
+// circle
+func TestPolygonCircle1(t *testing.T) {
+	queryBody := map[string]interface{}{
+		"query": map[string]interface{}{
+			"bool": map[string]interface{}{
+				"must": map[string]interface{}{
+					"match_all": struct{}{},
+				},
+				"filter": map[string]interface{}{
+					"geo_shape": map[string]interface{}{
+						"hotel.location": map[string]interface{}{
+							"shape": map[string]interface{}{
+								"type": "circle",
+								"radius": "500m",
+								"coordinates": []float64{116.49158, 39.97626},
 							},
 						},
 					},
