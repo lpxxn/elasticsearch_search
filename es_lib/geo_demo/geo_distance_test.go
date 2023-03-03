39b93e7304eb1d09f49c4af6dd29846bdade436a
lpxxn <mi_duo@live.com>
2023-03-03 13:18:16 +0800

    feat: demo

--- es_lib/geo_demo/geo_distance_test.go
+++ es_lib/geo_demo/geo_distance_test.go
@@ -18,3 +18,9 @@ func TestGeoDistinct(t *testing.T) {
 
 	fmt.Printf("The distance between London and Paris is %.2f km\n", distance/1000)
 }
+
+/*
+在这个示例中，我们使用了 s2.LatLngFromDegrees() 函数将经纬度转换为 s2.LatLng 类型，并将它们传递给 s2.PointFromLatLng() 函数来创建一个代表地球表面上一个点的 s2.Point 类型。
+然后，我们使用 Distance() 函数来计算两个点之间的距离，并将结果转换为以米为单位的距离。
+请注意，我们在距离计算中使用了地球的半径（6371 公里），这是一个常见的近似值。如果需要更高的精度，可以考虑使用更准确的地球半径值。
+*/
